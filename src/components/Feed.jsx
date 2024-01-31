dc4b1bfa049079a7d3553c3695704b3e632d9254
Ashen <dileepaashen81@gmail.com>
2024-02-01 00:50:38 +0530

    this is first commit
    example

---
+++ src/components/Feed.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Box} from "@mui/material"
+
+
+const Feed = ()=>{
+  return (
+    <Box bgcolor="pink" flex={4} p={2} marginLeft={10} marginRight={10}>
+      Feed
+    </Box>
+  )
+}
+
+export default Feed;
---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import SideBar from '../components/SideBar'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <div>
+        <SideBar></SideBar>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
